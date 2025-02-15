94ed3bde750b752c429475e0901190278dcaa2a1
Onni Nevala <nevalaonni@gmail.com>
2025-02-15 15:02:37 +0200

    added support for settings

--- src/main/java/site/frii/worldborder/Commands/Sync.java
+++ src/main/java/site/frii/worldborder/Commands/Sync.java
@@ -4,18 +4,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 import site.frii.worldborder.WorldborderManager;
 
 public class Sync implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, String[] args) {
         WorldborderManager instance = WorldborderManager.getInstance();
 
         int size = instance.getConfig().getInt("size");
         double amount = instance.getConfig().getDouble("amount");
 
         Bukkit.getWorld("world").getWorldBorder().setSize(size*amount);
-        commandSender.sendMessage(String.format("Setting worldborder to %s blocks",size*amount));
+        Bukkit.broadcastMessage(String.format("Setting world border to §b§l%s§r blocks.",size*amount));
 
         return true;
     }
